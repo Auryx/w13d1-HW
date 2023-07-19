e77f72bb45f06ae3e0c225e4d70fb62be3fee335
Auryx <lilbizb1@gmail.com>
2023-07-18 19:23:05 -0700

    end of class day1

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+// the component function
+const Header = (props) => {
+    //The Components Returned JSX
+    return <div className="Header">
+        <a href="https://www.linkedin.com/in/paul-bunda-iii/">LinkedIn</a>
+        <a href="https://github.com/Auryx">GitHub</a>
+        </div>
+}
+
+// export the component
+export default Header
+
